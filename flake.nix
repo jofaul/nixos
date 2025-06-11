7ea762c459d2ea963363dbe4191f9fb3c5f130a3
jofaul <54673768+jofaul@users.noreply.github.com>
2025-06-11 20:01:42 +0200

    initial

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+
+  description = "My first flake!";
+
+  inputs = {
+  
+    ### Essentials
+
+    # Give me the unstable packages!!
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        jonathan = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
+      };
+      homeConfigurations = {
+        jonathan = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "jonathan";
+  home.homeDirectory = "/home/jonathan";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    pkgs.hello
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -rtl";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "jofaul";
+    userEmail = "54673768+jofaul@users.noreply.github.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      safe.directory = "/etc/nixos";
+      safe.directory = "/home/jonathan/.dotfiles";
+    };
+  };
+
+}
