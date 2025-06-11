f5e034ad1fd53cf39692198ddb77ceb9af978e15
jofaul <54673768+jofaul@users.noreply.github.com>
2025-06-11 22:21:11 +0200

    tried some shit and still cant use keyboard in initrd so imma give up

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -12,17 +12,7 @@
   # Add the kernel modules such that we have a working keyboard for the 
   # LUKS full disk encryption.
   # https://github.com/linux-surface/linux-surface/wiki/Disk-Encryption
-  boot.initrd.kernelModules = [
-    "surface_aggregator"
-    "surface_aggregator_registry"
-    "surface_aggregator_hub"
-    "surface_hid_core"
-    "surface_hid"
-    "pinctrl_tigerlake"
-    "intel_lpss"
-    "intel_lpss_pci"
-    "8250_dw"
-  ];
+  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
