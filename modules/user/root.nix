473154bd314f6c98bcb08fd03038db93f770ac89
IAmAlife <54673768+IAmAlife@users.noreply.github.com>
2025-06-13 19:23:40 +0200

    trust noone

--- modules/user/root.nix
+++ modules/user/root.nix
@@ -13,13 +13,7 @@ in
 
   config = lib.mkIf cfg.enable {
     users.users.root = {
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMHjHjJ/vMoD3LId4IskuD9A6GwyTQXzt61IEq3/paul paulmiro@newton"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEom7NN1k9UCOTTiYsMBLqUK8BF8rjXTWAVQHSk/hwk paulmiro@hawking"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINirIGaLp4nLyJ4JUbCoknq5uqmp59uOfBrig0iW/mor paulmiro@morse"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINamsWVM0+zxXxUh5K1ww0GrdleakE3X8QSMJ+0b/btr paulmiro@better-laptop-paul-wsl"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB5CwYET0c/EEb9qOBUVB2MV4+yyLAlKgbEIvUuF/pxl paulmiro@pixel"
-      ];
+      # trust noone
     };
   };
 }
